18916cf56f656aef57bdfef12005f7656e7fbb2d
Tomasz Hermanowicz <hermangti@gmail.com>
2022-12-28 19:54:16 +0100

    Working Faker generator. Currently testing API gateway configs.

--- handler.rb
+++ handler.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'faker'
 
 def hello(event:, context:)
   res = JSON.dump({
@@ -8,7 +9,23 @@ def hello(event:, context:)
                       input: event.fetch('body', 'body-not-found')
                     }
                   })
-  return res
+  res
+end
+
+def rand_user(event:, context:)
+  res = JSON.dump({
+                    statusCode: 200,
+                    body: {
+                      message: 'Fake user',
+                      user: {
+                        f_name: Faker::Name.name,
+                        address: Faker::Address.full_address,
+                        currency: Faker::Currency.code,
+                        last_login: Faker::Date.backward(days: 21)
+                      }
+                    }
+                  })
+  res
 end
 
 def bye(event:, context:)
@@ -19,5 +36,5 @@ def bye(event:, context:)
                       input: event.fetch('body', 'body-not-found')
                     }
                   })
-  return res
+  res
 end
