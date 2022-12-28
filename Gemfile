e0d9844bad722737e9d2a57315c381406a8e1a18
Tomasz Hermanowicz <hermangti@gmail.com>
2022-12-28 19:16:24 +0100

    Second function added.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'sequel'
-gem 'postmark'
 gem 'aws-sdk'
--- handler.rb
+++ handler.rb
@@ -2,11 +2,22 @@ require 'json'
 
 def hello(event:, context:)
   res = JSON.dump({
-    statusCode: 200,
-    body: {
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event.fetch('body','body-not-found')
-    }
-  })
+                    statusCode: 200,
+                    body: {
+                      message: 'Go Serverless v1.0! Your function "hello" executed successfully!',
+                      input: event.fetch('body', 'body-not-found')
+                    }
+                  })
+  return res
+end
+
+def bye(event:, context:)
+  res = JSON.dump({
+                    statusCode: 200,
+                    body: {
+                      message: 'Go Serverless v1.0! Your function "bye" executed successfully!',
+                      input: event.fetch('body', 'body-not-found')
+                    }
+                  })
   return res
 end
